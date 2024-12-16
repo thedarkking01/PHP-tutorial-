e1c1ba6e5b78d1ab38fb5c780edfcb38233e902c
thedarkking01 <sourabhsinghbais52@gmail.com>
2024-12-16 13:37:34 +0530

    Constants in php

---
+++ constants.php
@@ -0,0 +1,13 @@
+<?php
+    // define("SITE_NAME", "MyWebsite");
+    // define("MAX_USERS", 100);
+    
+    // echo SITE_NAME; // Outputs: MyWebsite
+    // echo MAX_USERS; // Outputs: 100
+
+    const GREETING = "Welcome to PHP!";
+    const PI = 3.14159;
+
+    echo GREETING; // Outputs: Welcome to PHP!
+    echo PI; // Outputs: 3.14159
+?>
